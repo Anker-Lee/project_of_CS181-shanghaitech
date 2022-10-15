1feacf4edec8095c66c13e8b35d1835c37be767d
Anker-Lee <liza1@shanghaitech.edu.cn>
2022-10-15 11:30:19 +0800

    logic finished

---
+++ logic/test.py
@@ -0,0 +1,9 @@
+from logicPlan import *
+from logic import PropSymbolExpr, Expr, to_cnf
+print(findModel(sentence1()))
+
+# A = Expr('A')
+# B = Expr('B')
+# C = Expr('C')
+
+# print(to_cnf((A & B) >> C))
